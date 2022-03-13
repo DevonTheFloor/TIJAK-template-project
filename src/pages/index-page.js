6e6c6ee6595fa906aa22eef29459a5ad3d652943
DevonTheFloor <devonthefloor@free.fr>
2022-03-13 19:36:01 +0100

    Add link to page deux

--- src/pages/index-page.js
+++ src/pages/index-page.js
@@ -3,6 +3,7 @@ export default function indexPage (url) {
     <h1>Hello Vite!</h1>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
     <p>${url}</p>
+    <p>To page : <a href="./deux">DEUX</a></p>
   `
   return index;
   }
