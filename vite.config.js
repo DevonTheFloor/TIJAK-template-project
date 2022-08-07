fc26ecdfd55ffdc735f7cee890cbe8ac2c1d63ac
DevonTheFloor <devonthefloor@free.fr>
2022-08-07 20:33:34 +0200

    Add vite config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    open: 'index.html'
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "/src/sass/variables.scss";
+          @import "/src/sass/mixins.scss";`
+      }
+    }
+  },
+  
+})
+
