ef4519f72916b99c0dbad7f47e011974c4b7ea20
DevonTheFloor <devonthefloor@free.fr>
2022-08-30 17:01:37 +0200

    Fix scroll page to page

--- builders/page-navigation.js
+++ builders/page-navigation.js
@@ -1,8 +1,7 @@
-import { dGEBId, dQSr } from '../src/js/helpers/myDomHelper.js';
+import { dQSr } from '../src/js/helpers/myDomHelper.js';
 import { insertPageInApp } from './mounting-page.js';
 
 function navigator() {
-  //const mount = dGEBId('app'),
   const url = new URL(window.location.href),
     hach = url.hash,
     path = url.pathname;
@@ -10,32 +9,32 @@ function navigator() {
   switch(hach){
     case '' :
       console.log('hach vide: ', hach);
-      //mount.innerHTML = '<index-page></index-page>'; 
       insertPageInApp('<index-page></index-page>');
+      window.scroll(0, 0);
       break;
     case '#/' :
       console.log('hach vide: ', hach);
-      //mount.innerHTML = '';
-      //mount.innerHTML = '<index-page></index-page>'; 
+
       insertPageInApp('<index-page></index-page>');
+      window.scroll(0, 0);
 
       break;
     case '#/second-page':
-      //mount.innerHTML = '';
-      //mount.innerHTML = '<second-page></second-page>'
+
       insertPageInApp('<second-page></second-page>');
 
       break;
     case '#/error':
-      //mount.innerHTML = '';
-      //mount.innerHTML = '<error-404></error-404>';
+
       insertPageInApp('<error-404></error-404>');
+      window.scroll(0, 0);
       break;
       default:
         dQSr('#app').innerHTML = `
           <h1>ERROR 404</h1>
           <p>La page que vous cherchez n'existe pas</p>
-        `
+        `;
+        window.scroll(0, 0);
   }
 }
 export function activatedNavigator() {
